7dd3f58360915fa04735054392c6ec314fa50b01
felixsu <felix.soewito@gmail.com>
2016-04-16 19:20:02 +0700

    add CORS filter configuration

---
+++ src/main/java/com/felix/raspi/configuration/RestConfiguration.java
@@ -0,0 +1,28 @@
+package com.felix.raspi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by felix on 16/04/2016.
+ */
+
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
