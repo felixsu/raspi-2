cc2532553dcbb61f6842d92c2452a749cdcf059b
felixsu <felix.soewito@gmail.com>
2016-04-19 19:16:45 +0700

    logging fix (logback double appender) scheduler readjustment add timeout at weatherClient

--- src/main/java/com/felix/raspi/client/WeatherClient.java
+++ src/main/java/com/felix/raspi/client/WeatherClient.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -33,6 +34,12 @@ public class WeatherClient {
         RestTemplate restTemplate = new RestTemplate();
         Weather result;
         try {
+            SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+            simpleClientHttpRequestFactory.setConnectTimeout(5*1000);
+            simpleClientHttpRequestFactory.setReadTimeout(5*1000);
+
+            restTemplate.setRequestFactory(simpleClientHttpRequestFactory);
+
             result = restTemplate.getForObject(targetUrl, Weather.class);
         } catch (RestClientException e) {
             LOGGER.error("error call forecast.io API", e);
